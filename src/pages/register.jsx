b31d8cef75604dd1858a3f7b3bfd9df6f2b1205e
ganesh-48 <ganeshsrananavare@gmail.com>
2021-07-02 11:55:32 +0530

    feat: adding validate the registration form info

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,18 +2,23 @@ import react, { Component } from 'react';
 
 const formValid = ({ formErrors, ...rest }) => {
     let valid = true;
-  
+
     // validate form errors being empty
     Object.values(formErrors).forEach(val => {
-      val.length > 0 && (valid = false);
+        val.length > 0 && (valid = false);
     });
-    
-    return valid;
-  }
 
-  const emailIdRegex = RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
-  
-class App extends Component {
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+        val === null && (valid = false);
+    });
+
+    return valid;
+}
+
+const emailIdRegex = RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
+class UserRegistration extends Component {
 
     constructor(properties) {
         super(properties);
@@ -33,23 +38,27 @@ class App extends Component {
     }
 
     handleSubmit = e => {
+        /*alert('my name is ${this.state.firstName}.
+        my last is ${this.state.lastName}.
+        my eail is ${this.state.emailId}
+        my pass is ${this.state.password}');*/
         e.preventDefault();
-    
+
         if (formValid(this.state)) {
-          console.log(`
+            console.log(`
             First Name: ${this.state.firstName}
             Last Name: ${this.state.lastName}
             EmailId: ${this.state.emailId}
             Password: ${this.state.password}
           `);
         } else {
-          console.error("FORM INVALID ");
+            console.error("FORM INVALID ");
         }
-      };
+    };
 
-      handleChange = e => {
+    handleChange = e => {
         e.preventDefault();
-    
+
         const { name, value } = e.target;
         let formErrors = this.state.formErrors;
 
@@ -59,37 +68,40 @@ class App extends Component {
         switch (name) {
 
             case "firstName":
-              formErrors.firstName =
-                value.length < 3 && value.length > 0
-                  ? "minimum 3 characters are required"
-                  : "";
-              break;
-      
+                formErrors.firstName =
+                    value.length < 3
+                        ? "minimum 3 characters are required"
+                        : "";
+                break;
+
             case "lastName":
-              formErrors.lastName =
-                value.length < 3 && value.length > 0
-                  ? "minimum 3 characters are required"
-                  : "";
-              break;
-      
+                formErrors.lastName =
+                    value.length < 3
+                        ? "minimum 3 characters are required"
+                        : "";
+                break;
+
             case "emailId":
-              formErrors.emailId =
-                emailIdRegex.test(value) && value.length > 0
-                  ? ''
-                  : "Invalid Email address";
-              break;
-      
+                formErrors.emailId =
+                    emailIdRegex.test(value)
+                        ? ''
+                        : "Invalid Email address";
+                break;
+
             case "password":
-              formErrors.password =
-                value.length < 6 && value.length > 0
-                  ? "minimum 6 characters are required"
-                  : "";
-              break;
+                formErrors.password =
+                    value.length < 6 
+                        ? "minimum 6 characters are required"
+                        : "";
+                break;
             default:
-              break;
-      }
+                break;
+        }
+        this.setState({ formErrors, [name]: value }, () => console.log(this.state));
     }
+
     render() {
+        const { formErrors } = this.state;
         return (
             <div className="Employee-App">
                 <div className="Registration-Form">
@@ -98,22 +110,34 @@ class App extends Component {
                     <form onSubmit={this.handleSubmit} noValidate>
                         <div className='firstName'>
                             <label htmlFor='firstName'>First Name</label>
-                            <input type='text' className='' placeholder='firstName' name='firstName' onChange={this.handleChange}></input>
+                            <input type='text' className={formErrors.firstName.length > 0 ? "error" : null} placeholder='firstName' name='firstName' onChange={this.handleChange}></input>
+                            {formErrors.firstName.length > 0 && (
+                                <span className="errorMessage">{formErrors.firstName}</span>
+                            )}
                         </div>
 
                         <div className='lastName'>
                             <label htmlFor='lastName'>Last Name</label>
-                            <input type='text' className='' placeholder='lastName' name='lastName' onChange={this.handleChange}></input>
+                            <input type='text' className={formErrors.lastName.length > 0 ? "error" : null} placeholder='lastName' name='lastName' onChange={this.handleChange}></input>
+                            {formErrors.lastName.length > 0 && (
+                                <span className="errorMessage">{formErrors.lastName}</span>
+                            )}
                         </div>
 
                         <div className='emailId'>
                             <label htmlFor='emailId'>EmailId</label>
-                            <input type='text' className='' placeholder='emailId' name='emailId' onChange={this.handleChange}></input>
+                            <input type='text' className={formErrors.emailId.length > 0 ? "error" : null} placeholder='emailId' name='emailId' onChange={this.handleChange}></input>
+                            {formErrors.emailId.length > 0 && (
+                                <span className="errorMessage">{formErrors.emailId}</span>
+                            )}
                         </div>
 
                         <div className='password'>
                             <label htmlFor='password'>Password</label>
-                            <input type='text' className='' placeholder='password' name='password' onChange={this.handleChange}></input>
+                            <input type='text' className={formErrors.password.length > 0 ? "error" : null} placeholder='password' name='password' onChange={this.handleChange}></input>
+                            {formErrors.password.length > 0 && (
+                                <span className="errorMessage">{formErrors.password}</span>
+                            )}
                         </div>
 
                         <div className='RegistrationForm'>
@@ -127,4 +151,4 @@ class App extends Component {
     }
 }
 
-export default App
+export default UserRegistration
